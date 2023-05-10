53fb1fc075186d20729acaa10aa5606e3e9106ec
lbcanfield <lewie.b.canfield@gmail.com>
2023-05-10 10:21:17 -0500

    tabs.js -- Tabs function complete. -- tabsAppender function complete.

--- src/components/tabs.js
+++ src/components/tabs.js
@@ -1,28 +1,50 @@
+import axios from 'axios';
+
+
 const Tabs = (topics) => {
-  // TASK 3
-  // ---------------------
-  // Implement this function which takes an array of strings ("topics") as its only argument.
-  // As an example, if the topics passed are ['javascript', 'bootstrap', 'technology']
-  // then the function returns the markup below.
-  // The tags used, the hierarchy of elements and their attributes must match the provided markup!
-  // The text inside elements will be set using their `textContent` property (NOT `innerText`).
-  //
-  // <div class="topics">
-  //   <div class="tab">javascript</div>
-  //   <div class="tab">bootstrap</div>
-  //   <div class="tab">technology</div>
-  // </div>
-  //
+
+     const tabContainer = document.createElement('div');
+     tabContainer.classList.add('topics');
+
+     for (const topic of topics) {
+          const tab = document.createElement('div');
+          tab.classList.add('tab');
+          tab.textContent = topic;
+          tabContainer.appendChild(tab);
+     }
+
+     return tabContainer;
+     // TASK 3
+     // ---------------------
+     // Implement this function which takes an array of strings ("topics") as its only argument.
+     // As an example, if the topics passed are ['javascript', 'bootstrap', 'technology']
+     // then the function returns the markup below.
+     // The tags used, the hierarchy of elements and their attributes must match the provided markup!
+     // The text inside elements will be set using their `textContent` property (NOT `innerText`).
+     //
+     // <div class="topics">
+     //   <div class="tab">javascript</div>
+     //   <div class="tab">bootstrap</div>
+     //   <div class="tab">technology</div>
+     // </div>
+     //
 }
 
 const tabsAppender = (selector) => {
-  // TASK 4
-  // ---------------------
-  // Implement this function which takes a css selector as its only argument.
-  // It should obtain topics from this endpoint: `http://localhost:5001/api/topics` (test it with a console.log!).
-  // Find the array of topics inside the response, and create the tabs using the Tabs component.
-  // Append the tabs to the element in the DOM that matches the selector passed to the function.
-  //
+     // TASK 4
+     // ---------------------
+     // Implement this function which takes a css selector as its only argument.
+     // It should obtain topics from this endpoint: `http://localhost:5001/api/topics` (test it with a console.log!).
+     // Find the array of topics inside the response, and create the tabs using the Tabs component.
+     // Append the tabs to the element in the DOM that matches the selector passed to the function.
+     //
+     const obj = document.querySelector(selector)
+     axios.get('http://localhost:5001/api/topics')
+          .then(response => {
+               // console.log(response.data.topics)
+               document.querySelector(selector).appendChild(Tabs(response.data.topics))
+          })
+          .catch(error => console.log(error));
 }
 
 export { Tabs, tabsAppender }
