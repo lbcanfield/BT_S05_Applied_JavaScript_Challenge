19e0baabcb4e26499e60dc4797432f76be082d66
lbcanfield <lewie.b.canfield@gmail.com>
2023-05-10 09:41:41 -0500

    header.js -- Header variable function created.

--- src/components/header.js
+++ src/components/header.js
@@ -1,31 +1,45 @@
 const Header = (title, date, temp) => {
-  // TASK 1
-  // ---------------------
-  // Implement this function taking `title`, `date` and `temp` as its 3 args and returning the markup below.
-  // The html tags used, the hierarchy of elements and their attributes must match the provided markup exactly!
-  // The text inside elements will be set using their `textContent` property (NOT `innerText`).
-  //
-  //  <div class="header">
-  //    <span class="date">{ date }</span>
-  //    <h1>{ title }</h1>
-  //    <span class="temp">{ temp }</span>
-  //  </div>
-  //
+     const headerContainer = document.createElement('div').classList.add('header');
+     const dateElement = document.createElement('span').classList.add('date');
+     const titleElement = document.createElement('h1');
+     const tempElement = document.createElement('span').classList.add('temp');
+
+     headerContainer.appendChild(dateElement);
+     headerContainer.appendChild(titleElement);
+     headerContainer.appendChild(tempElement);
+
+     dateElement.textContent = date;
+     titleElement.textContent = title;
+     tempElement.textContent = temp;
+     // TASK 1
+     // ---------------------
+     // Implement this function taking `title`, `date` and `temp` as its 3 args and returning the markup below.
+     // The html tags used, the hierarchy of elements and their attributes must match the provided markup exactly!
+     // The text inside elements will be set using their `textContent` property (NOT `innerText`).
+     //
+     //  <div class="header">
+     //    <span class="date">{ date }</span>
+     //    <h1>{ title }</h1>
+     //    <span class="temp">{ temp }</span>
+     //  </div>
+     //
+     return headerContainer;
 }
 
-const headerAppender = (selector) => {
-  // TASK 2
-  // ---------------------
-  // Implement this function taking a css selector as its only argument.
-  // It should create a header using the Header component above, passing arguments of your choosing.
-  // It should append the header to the element in the DOM that matches the given selector.
-  //
 
-  // HINT: querySelector can take in a string (ie querySelector("#wrapper")) 
-  // but it can also take in a variable (ie querySelector(selector))
-  // We are taking care of passing in the correct selector on line 16,
-  // so all that you need to do is pass it into the querySelector method
-  // for the tests to work!
+const headerAppender = (selector) => {
+     // TASK 2
+     // ---------------------
+     // Implement this function taking a css selector as its only argument.
+     // It should create a header using the Header component above, passing arguments of your choosing.
+     // It should append the header to the element in the DOM that matches the given selector.
+     //
+
+     // HINT: querySelector can take in a string (ie querySelector("#wrapper")) 
+     // but it can also take in a variable (ie querySelector(selector))
+     // We are taking care of passing in the correct selector on line 16,
+     // so all that you need to do is pass it into the querySelector method
+     // for the tests to work!
 }
 
 export { Header, headerAppender }
